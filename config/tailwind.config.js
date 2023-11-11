2ab0406739eff4586ceab2bbac9cd8f711358e8d
RODRIGO <rodrigo_torres007@hotmail.com>
2023-11-11 12:45:57 +0100

    Crea Repo Git

---
+++ config/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  prefix: "tw-",
+  // important: true,
+  // corePlugins: {
+  //   preflight: false,
+  // },
+};
---
+++ js/tailwindcss.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+  prefix: "tw-",
+//   corePlugins: {
+//     preflight: false,
+//   },
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+      },
+    },
+  },
+};
